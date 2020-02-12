90ebd474b6afe42bf8340bc7482ca0814308c41d
Josh <jvandenbrink1782@gmail.com>
2020-02-12 11:54:53 -0700

    VSC exercise1 1st save

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace C__Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Assigned an int!
+            int x = 25;
+            Console.WriteLine("Hello World!"+x);
+
+            if ( x < 30 ) {
+            Console.WriteLine( "X is less than thrity!" );
+            } else if ( x == 30 ) {
+                Console.WriteLine( "x is thirty!" );
+            } else { 
+                Console.WriteLine( "x is greater than thirty" );
+            }
+
+             int a = 2;
+             switch ( a ) {
+                case 1 :
+                    Console.WriteLine( "Value is 1!" );
+                    break;
+                case 2 :
+                    Console.WriteLine ( "Value is 2!" );
+                    break;
+                case 3 :
+                    Console.WriteLine( "Value is 3!" );
+                    break;
+                // Pay attention to opening and closing with curly brackets
+                default:
+                    Console.WriteLine( "uh oh, it is another value" );
+                    break;
+
+             }
+        }
+    }
+}
